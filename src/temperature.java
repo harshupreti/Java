adacf7315a0c7d42d8c1a31a5da1a117843be681
Harsh Upreti <harsh.upreti123@gmail.com>
2024-01-08 18:39:17 +0530

    Temperature

---
+++ src/temperature.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class temperature {
+    public static void main(String[] args)
+    {
+        System.out.print("Enter the value in fahrenheit: ");
+
+        Scanner input = new Scanner(System.in);
+
+        double fahrenheit = input.nextDouble();
+
+        double celsius = (5.0 / 9) * (fahrenheit - 32);
+
+        System.out.print("Temperature in celsius is: " + celsius);
+    }
+}

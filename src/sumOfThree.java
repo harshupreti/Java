fd84468500b62bdf2e766b2673a89343916e0ada
Harsh Upreti <harsh.upreti123@gmail.com>
2024-02-17 09:29:14 +0530

    Sum of 3 no.s

---
+++ src/sumOfThree.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class sumOfThree {
+    public static void main(String[] args) {
+        System.out.println("Enter 3 numbers");
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.print("Sum of three numbers is: " + (a+b+c));
+    }
+}
